5759b5522530257f0791c5531357e792f5e30e50
Tanjim Noor <tanjim.noor@g.bracu.ac.bd>
2025-01-08 15:51:50 +0600

    ui to add note

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,67 @@
 import "./App.css";
+import { useState } from "react";
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+};
 
 const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    { id: 1, title: "note title1", content: " content1" },
+    { id: 2, title: "note title 2", content: " content2" },
+    { id: 3, title: "note title 3", content: " content3" },
+    { id: 4, title: "note title 4", content: " content4" },
+    { id: 5, title: "note title 5", content: " content5" },
+  ]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("tittle: ", title);
+    console.log("content: ", content);
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content,
+    };
+    setNotes([newNote, ...notes]);
+    setTitle(""), setContent("");
+  };
+
   return (
     <div className="app-container">
-      <form className="note-form">
-        <input placeholder="tittle" required />
-        <textarea placeholder="Content" rows={1} required />
+      <form className="note-form" onSubmit={(event) => handleSubmit(event)}>
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          placeholder="Content"
+          rows={10}
+          required
+        />
 
         <button type="submit">Add Note</button>
       </form>
-
       <div className="notes-grid">
-        <div className="note-item">
-          <div className="notes-header">
-            <button>x</button>
+        {notes.map((note) => (
+          <div className="note-item" key={note.id}>
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
-          <h2>Note tittle</h2>
-          <p>Note content</p>
-        </div>
+        ))}
       </div>
     </div>
   );
 };
-export default App
+export default App;
