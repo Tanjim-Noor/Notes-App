063f47e3b8388fbbc18fa2c747493f2c7ac3a6ec
Tanjim Noor <tanjim.noor@g.bracu.ac.bd>
2025-01-11 07:16:03 +0600

    added routes for put(edit) and delete(modify)

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,15 +17,59 @@ app.get("/api/notes", async (req, res) => {
 app.post("/api/notes", async (req, res) => {
   const { title, content } = req.body;
 
-  // if (!title || !content){
-  //   return res. status(400).send("title and content fields required");
-  // }
+  if (!title || !content) {
+    res.status(400).send("title and content fields required");
+  }
 
-  const note = await prisma.note.create({
-    data: { title, content },
-  });
-  res.json(note);
-})
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Something is wrong figure it out!");
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if (!title || !content) {
+    res.status(400).send("title and content fields required");
+  }
+
+  if (!id || isNaN(id)) {
+    res.status(400).send("ID must be a valid number");
+  }
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).send("Something is wrong figure it out!");
+  }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    res.status(400).send("ID field required");
+  }
+
+  try {
+    await prisma.note.delete({
+      where: { id },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send("Something is wrong figure it out!");
+  }
+});
 
 app.listen(5000, () => {
   console.log("server running on localhost:5000");
